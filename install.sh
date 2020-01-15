5a6270297d0acf4809499537e3653571b7d7dff6
Ilya Rusin <franchb@protonmail.ch>
2020-01-15 17:35:07 +0000

    added .profile

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ wget https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
 tar -xvf go1.13.6.linux-amd64.tar.gz
 sudo mv go /usr/local
 
-echo 'export APP=/opt/tinyos-2.x/apps' >> ~/.bashrc 
-
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc 
 echo 'export GOPATH=$HOME/workspace' >> ~/.bashrc 
 echo 'export PATH=$GOROOT/bin:$GOPATH/bin:$PATH' >> ~/.bashrc
